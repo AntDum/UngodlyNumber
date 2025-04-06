8afda1f3a6af1a77801f7f7f46a6df6de9622056
AntDum <antoine.dumoulin@student.uclouvain.be>
2025-04-06 15:53:43 +0200

    init

---
+++ singletons/scene_manager.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+@export var dead_time : float = 0.2 
+var _currently_loading : bool = false
+var _scene_to_load : String = ""
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+func swap_scene(to: String) -> void:
+	if _currently_loading: return
+	if not ResourceLoader.exists(to): return
+	_scene_to_load = to
+	_start_transition()
+
+func _start_transition() -> void:
+	_currently_loading = true
+	animation_player.play("ripple_in")
+	
+func _transition() -> void:
+	get_tree().change_scene_to_file(_scene_to_load)
+
+func _end_transition() -> void:
+	_currently_loading = false
+
+func _finished_fade_in() -> void:
+	timer.wait_time = dead_time
+	timer.one_shot = true
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	animation_player.play("ripple_out")
+	_end_transition()
